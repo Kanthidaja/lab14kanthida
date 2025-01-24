a2ca22733f526e8adf8b76bf80300304a52037c7
kanthidaja <kanthida_khun@cmu..ac.th>
2025-01-24 16:03:17 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 
+template<typename T>
+void swap(T d[],int x,int y){
+	T temp = d[x];
+	d[x] = d[y];
+	d[y] = temp;
+} 
+
 template <typename T>
 void insertionSort(T d[],int N){
-
+ for(int i=1;i<N;i++){  //10ตัวแปร ทำ9รอบ รอบ1เทียบ1 รอบ2เทียบ2
+    for(int j=i;j>0;j--){  //1 0,2 1 0,3 2 1 0 
+	 {
+      if(d[j]>d[j-1]){ //i=1 j[1]=
+      swap(d,j,j-1);
+	  }
+	 }
+	}
+	cout << "Pass " << i << ":" ;
+	for(int k=0;k<N;k++){
+		cout << d[k] << " ";
+	}
+	cout << endl;
+ }
 }
 
 int main(){
